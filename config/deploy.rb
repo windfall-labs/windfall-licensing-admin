b662f1fada645f2559e194f4373bc9a287cb54d0
Mark <markaventura@gmail.com>
2016-08-10 22:17:58 +0800

    WIP: deployment setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :bundle_without, [:development, :test]
 
 set :log_level, :debug
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 1
 set :bundle_gemfile, -> { release_path.join('Gemfile') }
