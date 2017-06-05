8618b44da4c94040af14af40e1f43c35190ec6d3
Thorpe Obazee <forum.providence@yahoo.com>
2017-06-05 20:14:33 +0800

    [FEATURE] Added Receipt Rejected Count to search area

--- app/models/rsd_product.rb
+++ app/models/rsd_product.rb
@@ -3,7 +3,7 @@ class RsdProduct < ActiveRecord::Base
   establish_connection "product_control_#{Rails.env}".to_sym
 
 
-  def self.filter(search_text, receipt_accepted_count, unique_rejected_count, unique_accepted_count, accepted_count, rejected_count, alt_product_accepted_count, alt_product)
+  def self.filter(search_text, receipt_accepted_count, unique_rejected_count, unique_accepted_count, accepted_count, rejected_count, alt_product_accepted_count, alt_product, receipt_rejected_count)
     sql = []
 
     sql << "rsd ILIKE ?" if search_text.present?
@@ -14,6 +14,7 @@ class RsdProduct < ActiveRecord::Base
     sql << "rejected_count = ?" if rejected_count.present?
     sql << "alt_product_accepted_count = ?" if alt_product_accepted_count.present?
     sql << "alt_product = ?" if alt_product.present?
+    sql << "receipt_rejected_count = ?" if receipt_rejected_count.present?
 
     values = []
 
@@ -25,6 +26,7 @@ class RsdProduct < ActiveRecord::Base
     values << rejected_count if rejected_count.present?
     values << alt_product_accepted_count if alt_product_accepted_count.present?
     values << alt_product if alt_product.present?
+    values << receipt_rejected_count if receipt_rejected_count.present?
 
 
     where(sql.join(" AND "), *values)
