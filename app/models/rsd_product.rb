388d557086f234a33f4936588886e4ce6235599f
Thorpe Obazee <forum.providence@yahoo.com>
2017-06-05 20:32:20 +0800

    [FEATURE] make sure that integers are passed to sql statement when needing integers

--- app/models/rsd_product.rb
+++ app/models/rsd_product.rb
@@ -19,14 +19,14 @@ class RsdProduct < ActiveRecord::Base
     values = []
 
     values << "%#{search_text}%" if search_text.present?
-    values << receipt_accepted_count if receipt_accepted_count.present?
-    values << unique_rejected_count if unique_rejected_count.present?
-    values << unique_accepted_count if unique_accepted_count.present?
-    values << accepted_count if accepted_count.present?
-    values << rejected_count if rejected_count.present?
-    values << alt_product_accepted_count if alt_product_accepted_count.present?
+    values << receipt_accepted_count.to_i if receipt_accepted_count.present?
+    values << unique_rejected_count.to_i if unique_rejected_count.present?
+    values << unique_accepted_count.to_i if unique_accepted_count.present?
+    values << accepted_count.to_i if accepted_count.present?
+    values << rejected_count.to_i if rejected_count.present?
+    values << alt_product_accepted_count.to_i if alt_product_accepted_count.present?
     values << alt_product if alt_product.present?
-    values << receipt_rejected_count if receipt_rejected_count.present?
+    values << receipt_rejected_count.to_i if receipt_rejected_count.present?
 
 
     where(sql.join(" AND "), *values)
